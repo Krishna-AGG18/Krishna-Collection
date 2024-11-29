70a59d258b647d9a07fc9d394745f66af2dbc3e0
KRISHNA <kraggr2909@gmail.com>
2024-11-29 18:48:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ document.getElementById("menu-toggle").addEventListener("click",()=>{
     document.getElementById("menu-toggle").classList.toggle('fa-xmark');
     const menuToggle = document.getElementById("menu-toggle");
 
-    // Toggle 'left' property value between 0 and 175px
-    if (menuToggle.style.left === "16vw") {
-        menuToggle.style.left = "0px";  // Reset to 0px (or any other starting value)
+    if (menuToggle.style.left === "230px") {
+        menuToggle.style.left = "5px";  // Reset to 0px (or any other starting value)
         menuToggle.style.transitionDuration = "0.3s"
     } else {
         menuToggle.style.transitionDuration = "0.5s"
-        menuToggle.style.left = "16vw";  // Move it to 175px
+        menuToggle.style.left = "230px";  
     }
-    // if(screen.width <= "400px")
-    // {
-    //     menuToggle.style.left = "168px";
-    // }
+
     const menu = document.getElementById('menu');   
     menu.classList.toggle('open')
 });
