1ad0923ba9bff52e42306106845b89d78ba19509
KRISHNA <kraggr2909@gmail.com>
2024-11-29 11:03:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ document.getElementById("menu-toggle").addEventListener("click",()=>{
     const menuToggle = document.getElementById("menu-toggle");
 
     // Toggle 'left' property value between 0 and 175px
-    if (menuToggle.style.left === "225px") {
+    if (menuToggle.style.left === "16vw") {
         menuToggle.style.left = "0px";  // Reset to 0px (or any other starting value)
         menuToggle.style.transitionDuration = "0.3s"
     } else {
         menuToggle.style.transitionDuration = "0.5s"
-        menuToggle.style.left = "225px";  // Move it to 175px
+        menuToggle.style.left = "16vw";  // Move it to 175px
     }
+    // if(screen.width <= "400px")
+    // {
+    //     menuToggle.style.left = "168px";
+    // }
     const menu = document.getElementById('menu');   
     menu.classList.toggle('open')
 });
