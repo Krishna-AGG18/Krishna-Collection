aaf5a74c0c7c135b6950eb7e732aff174dbb77c5
KRISHNA <kraggr2909@gmail.com>
2024-12-12 12:47:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementById("menu-toggle").addEventListener("click",()=>{
     document.getElementById("menu-toggle").classList.toggle('fa-xmark');
     const menuToggle = document.getElementById("menu-toggle");
-
+    
     if (menuToggle.style.left === "230px") {
         menuToggle.style.left = "5px";  // Reset to 0px (or any other starting value)
         menuToggle.style.transitionDuration = "0.3s"
